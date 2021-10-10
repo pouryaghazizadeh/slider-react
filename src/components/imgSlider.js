b8c3704a63eeedf752f0ed950d5cfeb504a001ff
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-10-10 12:48:40 +0200

    make data

---
+++ src/components/imgSlider.js
@@ -0,0 +1,26 @@
+
+import First from "../assets/First.jpg"
+import  Second from "../assets/Second.jpg"
+import Tird from "../assets/Third.jpg"
+
+export defult [{
+    title:"helpppp",
+    dis:"fbjdbjekbhjrbvkjdsbvuieirbj",
+    url:First
+
+},
+{
+    title:"hellooooooooo",
+    dis:"abcd efnjkfr",
+    url: Second
+
+},
+
+    {
+        title:"bayyyyyyy",
+        dis:"i am pourthjd",
+        url: Tird 
+    
+    }
+
+]
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
 
 
@@ -10,4 +9,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
